a2ef43104b0a29509424bd92be5abe19d36c7f02
Nam Seob Seo <nsclass@gmail.com>
2021-12-31 22:34:55 -0600

    error handling

--- calculator-service/src/main.rs
+++ calculator-service/src/main.rs
@@ -1,3 +1,11 @@
 
+use calculator_engine::calculate_str;
+
 fn main() {
+    let input = "1 + 2";
+    let result = calculate_str(input);
+    match result {
+        Ok(val) => println!("{} is {}", input, val),
+        Err(err) => println!("{}, error: {:?}", input, err)
+    }
 }
