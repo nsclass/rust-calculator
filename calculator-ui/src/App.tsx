0c5f644a35373a9f735566bcaf3b83cb9456fd92
Nam Seob Seo <nsclass@gmail.com>
2024-05-03 22:19:15 -0500

    first working UI

--- calculator-ui/src/App.tsx
+++ calculator-ui/src/App.tsx
@@ -1,10 +1,50 @@
+import {useState} from "react";
+
+const CalculateText = () => {
+  const [calculateText, setCalculateText] = useState<string>("1+2")
+  const [calculationResult, setCalculationResult] = useState({})
+
+  const calculateRequest = async () => {
+
+    const payload = {
+      infix: calculateText
+    }
+
+    const response = await fetch('/calculate', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(payload)
+    })
+    const result = await response.json()
+    setCalculationResult(result)
+  }
+
+  return (
+    <div className="w-1/2 h-20">
+      <div className="flex w-full h-1/2">
+        <input className="border-2 rounded-l-2xl w-full h-full text-center" type="text" placeholder="1 + 2"
+               value={calculateText}
+               onChange={e => setCalculateText(e.target.value)}/>
+        <button className="border-2 rounded-r-2xl" onClick={calculateRequest}>Calculate</button>
+      </div>
+      <div className="flex w-full h-full">
+        <textarea className="border-2 w-full "
+                  value={JSON.stringify(calculationResult, null, 2)}
+
+        />
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="flex justify-center h-screen items-center">
+      <CalculateText/>
+    </div>
   )
 }
 
