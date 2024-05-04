d1b31293d5c9ed480d21d09d4543e189660ba56b
Nam Seob Seo <nsclass@gmail.com>
2024-05-03 22:33:41 -0500

    updated UI

--- calculator-ui/src/App.tsx
+++ calculator-ui/src/App.tsx
@@ -1,8 +1,19 @@
 import {useState} from "react";
 
+const DisplayCalculation = ({calculationResult} : { calculationResult: any}) => {
+  return (
+    <div className="flex flex-col w-full h-auto">
+      <label className="h-10">{`Result: ${calculationResult?.result ?? ""}`}</label>
+        <textarea className="border-2 w-full h-screen "
+                  value={JSON.stringify(calculationResult, null, 2)}
+        />
+    </div>
+  )
+}
+
 const CalculateText = () => {
   const [calculateText, setCalculateText] = useState<string>("1+2")
-  const [calculationResult, setCalculationResult] = useState({})
+  const [calculationResult, setCalculationResult] = useState()
 
   const calculateRequest = async () => {
 
@@ -23,26 +34,21 @@ const CalculateText = () => {
   }
 
   return (
-    <div className="w-1/2 h-20">
-      <div className="flex w-full h-1/2">
+    <div className="mt-10 w-2/3 h-auto">
+      <div className="flex w-full h-10">
         <input className="border-2 rounded-l-2xl w-full h-full text-center" type="text" placeholder="1 + 2"
                value={calculateText}
                onChange={e => setCalculateText(e.target.value)}/>
         <button className="border-2 rounded-r-2xl" onClick={calculateRequest}>Calculate</button>
       </div>
-      <div className="flex w-full h-full">
-        <textarea className="border-2 w-full "
-                  value={JSON.stringify(calculationResult, null, 2)}
-
-        />
-      </div>
+      <DisplayCalculation calculationResult={calculationResult} />
     </div>
   )
 }
 
 function App() {
   return (
-    <div className="flex justify-center h-screen items-center">
+    <div className="flex justify-center h-screen">
       <CalculateText/>
     </div>
   )
