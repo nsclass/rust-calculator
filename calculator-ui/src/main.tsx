b49743fc54a703d2df12a8a258b2638b005c7c22
Nam Seob Seo <nsclass@gmail.com>
2024-07-09 15:03:38 -0500

    feature: configured nextui for tailwnd

--- calculator-ui/src/main.tsx
+++ calculator-ui/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/i18n.ts";
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 );
