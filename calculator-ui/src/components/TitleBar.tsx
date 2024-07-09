c9a7ba4d20c556b4ce30551ad2fcdfa3d23df7a5
Nam Seo <nsclass@gmail.com>
2024-07-09 15:04:31 -0500

    feature: configured nextui for tailwnd (#2)

--- calculator-ui/src/components/TitleBar.tsx
+++ calculator-ui/src/components/TitleBar.tsx
@@ -4,16 +4,16 @@ import { LangSelector } from "./LangSelector";
 export const TitleBar = () => {
   const { t } = useTranslation();
   return (
-    <div className="flex items-end items-center content-center w-full border-solid border-2 h-20">
-      <div className="flex items-center border-2 w-4/5 m-3 p-2">
+    <div className="flex items-end items-center content-center w-full h-20">
+      <div className="flex items-center w-4/5 m-3 p-2">
         <h1 className="text-2xl w-full ml-10 text-center">
           {t("calculator.title")}
         </h1>
-        <h1 className="text-xs w-60">
+        <h1 className="text-xs w-1/5">
           {t("calculator.date", { date: new Date() })}
         </h1>
       </div>
-      <div className="w-1/5 p-1 text-center border-2">
+      <div className="w-1/5 p-1 text-center">
         <LangSelector />
       </div>
     </div>
